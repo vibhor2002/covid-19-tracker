141f3e20fce78a7b17e2d2b8d8adc71817c05b84
Vibhor Grover <grovervibhor21@gmail.com>
2022-04-10 23:09:05 +0530

    using map function ro loop through all countries

--- src/App.js
+++ src/App.js
@@ -9,21 +9,41 @@ import {
 } from "@mui/material"
 
 function App() {
-  
-  // https://disease.sh/v3/covid-19/countries
-  
-  // USE EFFECT = Runs a piece of code based on a given condition 
-  useEffect(() => {
-    // The code inside here run once
-    // when the component loads and not again after   
-  }, [countries])
-  
-  
+
   // STATE = How to write variable in react
   const [countries, setCountries] = useState([
     'USA', 'UK', 'INDIA'
   ]);
 
+  // https://disease.sh/v3/covid-19/countries
+
+  // USE EFFECT = Runs a piece of code based on a given condition 
+  useEffect(() => {
+
+    
+    // The code inside here run once
+    // when the component loads and not again after   
+    
+    // async -> sends a request , wait for it , and do something with the information
+    
+    const getCountriesData = async () =>{
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response)=>response.json())
+      .then((data)=> {
+
+        const countries = data.map((country)=>(
+          {
+            name: country.country, // United States, United Kingdom 
+            value: country.countryInfo.iso2 //UK, USA 
+          }
+          ));
+          setCountries(countries);
+        })
+      }
+
+      getCountriesData();
+    }, [])
+    
 
   return (
     <div className="app">
@@ -35,7 +55,7 @@ function App() {
 
             {
               countries.map(country => (
-                <MenuItem value={country}>{country}</MenuItem>
+                <MenuItem value={country.value}>{country.name}</MenuItem>
               ))
             }
 
